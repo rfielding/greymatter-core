a4ea1a16a58bfa77987080fef2eaf9bb1bc3d47b
moosecanswim <kyle.gallagher.09@gmail.com>
2022-11-30 13:22:18 -0500

    remove fips from generic names

--- scripts/greymatter-download-template.sh
+++ scripts/greymatter-download-template.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 artifacts=(
-        "https://greymatter.jfrog.io/artifactory/generic/greymatter-cli/greymatter-cli_<VERSION>-fips_linux_amd64.tar.gz"
+        "https://greymatter.jfrog.io/artifactory/generic/greymatter-cli/greymatter-cli_<VERSION>_linux_amd64.tar.gz"
         "https://greymatter.jfrog.io/artifactory/generic/greymatter-catalog/greymatter-catalog_<VERSION>_linux_amd64.tar.gz"
-        "https://greymatter.jfrog.io/artifactory/generic/greymatter-control/greymatter-control_<VERSION>-fips_linux_amd64.tar.gz"
-        "https://greymatter.jfrog.io/artifactory/generic/greymatter-control-api/greymatter-control-api_<VERSION>-fips_linux_amd64.tar.gz"
+        "https://greymatter.jfrog.io/artifactory/generic/greymatter-control/greymatter-control_<VERSION>_linux_amd64.tar.gz"
+        "https://greymatter.jfrog.io/artifactory/generic/greymatter-control-api/greymatter-control-api_<VERSION>_linux_amd64.tar.gz"
         "https://greymatter.jfrog.io/artifactory/generic/greymatter-dashboard/greymatter-dashboard_<VERSION>_linux_amd64.tar.gz"
-        "https://greymatter.jfrog.io/artifactory/generic/greymatter-proxy/greymatter-proxy_<VERSION>-fips-ubuntu_linux_amd64.tar.gz"
+        "https://greymatter.jfrog.io/artifactory/generic/greymatter-proxy/greymatter-proxy_<VERSION>-ubuntu_linux_amd64.tar.gz"
         "https://greymatter.jfrog.io/artifactory/generic/greymatter-core/greymatter-core_<VERSION>_none_none.tar.gz"
         "https://greymatter.jfrog.io/artifactory/generic/greymatter-audits/greymatter-audits_<VERSION>_linux_amd64.tar.gz"
 )
@@ -23,9 +23,9 @@ if [[ -z "$USERNAME" ]] || [[ -z "$API_KEY" ]]; then
 fi
 
 
-# for artifact in "${artifacts[@]}"; do
-#     echo "downloading $artifact"
-#     curl -s -L -u $USERNAME:$API_KEY \
-#         $artifact \
-#         -o $( echo $artifact | cut -d/ -f7 )
-# done
+for artifact in "${artifacts[@]}"; do
+    echo "downloading $artifact"
+    curl -s -L -u $USERNAME:$API_KEY \
+        $artifact \
+        -o $( echo $artifact | cut -d/ -f7 )
+done
