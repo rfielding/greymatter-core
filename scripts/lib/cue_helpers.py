28851c93404fee74016c304b205f2a88b25093e2
Coleman McFarland <coleman@de.limited>
2023-03-31 16:40:12 -0400

    PEP8 Format: scripts/lib/cue_helpers.py

--- scripts/lib/cue_helpers.py
+++ scripts/lib/cue_helpers.py
@@ -1,25 +1,27 @@
-import subprocess
 import logging
+import subprocess
 
 LOGGER = logging.getLogger(__name__)
+
 
 # The way that we shell out to cue makes it dificult to write tests for
 # These need to be "trusted" but we treat the json output as the interface
 def cue_eval(tag_dict):
-    eval_cmd="cue eval -c ./inputs.cue ./global_intermediates.cue ./secrets.cue --out=json"
+    eval_cmd = "cue eval -c ./inputs.cue ./global_intermediates.cue ./secrets.cue --out=json"
     LOGGER.info("Evaluating cue to create JSON")
     for i in tag_dict:
-        eval_cmd="%s -t %s=%s" % (eval_cmd, i, tag_dict[i])
-    r=""
+        eval_cmd = "%s -t %s=%s" % (eval_cmd, i, tag_dict[i])
+    r = ""
     LOGGER.debug("Cue eval command: %s " % eval_cmd)
     try:
-        r=getProcessOutput(eval_cmd)
+        r = getProcessOutput(eval_cmd)
     except subprocess.CalledProcessError as e:
         LOGGER.error("could not render json from cue eval string provided. \n%s" % e)
         raise e
-    
+
     LOGGER.info("Done Evaluating cue to create JSON")
     return r
+
 
 def getProcessOutput(cmd):
     process = subprocess.Popen(
